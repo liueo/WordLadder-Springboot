108484058514efea3105287252814783896d0132
DESKTOP-F109P7R\loluz <zy409815681@qq.com>
2018-04-02 23:30:52 +0800

    finish

---
+++ src/main/java/com/spring/service/controller/Controller.java
@@ -0,0 +1,29 @@
+package com.spring.service.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import com.spring.service.domain.WordLadder;
+import java.util.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@RestController
+public class Controller {
+    protected final Logger log =LoggerFactory.getLogger(this.getClass());
+    @RequestMapping
+    public String index(){
+        return "Hello World!";
+    }
+
+    @RequestMapping(value="/wordladder")
+    public Stack<String> Res(@RequestParam(required=true)String w1,
+                      @RequestParam(required=true)String w2){
+        log.debug("Generating ladder between "+w1+" and "+w2+"\n");
+        WordLadder wl = new WordLadder();
+        log.debug(("Finish\n"));
+        return wl.findLadder(w1,w2);
+
+    }
+
+}
